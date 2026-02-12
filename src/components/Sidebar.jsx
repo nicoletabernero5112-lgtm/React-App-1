ff070e23273c3150dc1704dc63aad6b1370b2a9a
nicoletabernero5112-lgtm <nicole.tabernero5112@gmail.com>
2026-02-12 17:41:44 -0500

    react-application-app

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,14 +4,19 @@ import { useState, useCallback } from "react"
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar({ initialMenuItems}) {
   let [newMenuItem, setNewMenuItem] = useState("")
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
   // let [menuItems, setMenuItems] = useState(initialMenuItems)
+  let [menuItems, setMenuItems] = useState(initialMenuItems)
   let [filter, setFilter] = useState("")
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
+    if (newMenuItem.trim() == "") {
+      setMenuItems([newMenuItem, ...menuItems]);
+      setNewMenuItem("");
+    }
     console.log("Added menu item")
     //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
     //   // This involves adding a parameter and changing a class instance variable (props).
@@ -34,7 +39,7 @@ export default function Sidebar() {
       <br />
       <button
         onClick={() => {
-          /* TODO: 3 */
+          addMenuItem();
         }}
       >
         Add Item
@@ -47,6 +52,14 @@ export default function Sidebar() {
         onChange={(event) => setFilter(event.target.value)}
         placeholder="Filter by..."
       ></input>
+      <ul>
+        {menuItems
+          .filter((item) => item.toLowerCase().includes(filter.toLowerCase()))
+          //filters items, should be case insensitive
+          .map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+      </ul>
     </div>
   )
 }
